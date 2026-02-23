e2bede54ba41121d0595e40a75d3ee00a0d0c15c
Dan Baskette <dbbaskette@gmail.com>
2026-02-22 23:14:26 -0500

    IssueBot: Define DropshipProperties configuration (#4) (#28)
    Co-authored-by: dbbaskette <dbbaskette@localhost>

--- src/main/java/com/baskette/dropship/DropshipApplication.java
+++ src/main/java/com/baskette/dropship/DropshipApplication.java
@@ -1,9 +1,12 @@
 package com.baskette.dropship;
 
+import com.baskette.dropship.config.DropshipProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DropshipProperties.class)
 public class DropshipApplication {
 
     public static void main(String[] args) {
